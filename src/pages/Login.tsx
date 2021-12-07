85ffef3fd54f91ce0406e90e17bc14315fb27d17
PrinceNiyonshuti <npprince47@gmail.com>
2021-12-07 12:14:48 +0200

    bg done

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -45,8 +45,8 @@ function Login() {
 					<div className="h-32 md:h-auto md:w-1/2">
 						<img
 							className="object-cover w-full h-full"
-							src="https://source.unsplash.com/user/erondu/1600x900"
-							alt="img"
+							src="/bg.jpg"
+							alt="Movie App Background"
 						/>
 					</div>
 					<div className="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
@@ -89,7 +89,7 @@ function Login() {
 								</button>
 							</form>
 							<p className="mt-4">
-								Need an account ?
+								Need an account ? &nbsp;
 								<Link
 									to="Register"
 									className="text-sm text-blue-600 hover:underline">
@@ -100,16 +100,10 @@ function Login() {
 
 							<div className="flex items-center justify-center gap-4">
 								<button className="flex items-center justify-center w-full px-4 py-2 text-sm  border border-gray-300 rounded-lg hover:border-gray-500 focus:border-gray-500">
-									<svg
-										className="w-4 h-4 mr-2"
-										aria-hidden="true"
-										viewBox="0 0 24 24"
-										fill="currentColor">
-										<path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
-									</svg>
-									Github
+									<img src="https://img.icons8.com/color/20/000000/google-logo.png" />{" "}
+									&nbsp; Google
 								</button>
-								<button className="flex items-center justify-center w-full px-4 py-2 text-sm text-white border border-gray-300 rounded-lg hover:border-gray-500 focus:border-gray-500">
+								<button className="flex items-center justify-center w-full px-4 py-2 text-sm border border-gray-300 rounded-lg hover:border-gray-500 focus:border-gray-500">
 									<svg
 										className="w-4 h-4 mr-2"
 										aria-hidden="true"
