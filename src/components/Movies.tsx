709bfaabdf7d99919f46b9851d2b544f243efdb3
PrinceNiyonshuti <npprince47@gmail.com>
2021-12-08 20:44:40 +0200

    changed svg to cursor pointer

--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -30,7 +30,7 @@ const Movies = (props: IMovie) => {
 								<svg
 									onDoubleClick={() => handleFavorite(movie.id, movie.favorite)}
 									xmlns="http://www.w3.org/2000/svg"
-									className="h-6 w-6 float-right"
+									className="h-6 w-6 float-right cursor-pointer"
 									viewBox="0 0 24 24"
 									fill="black">
 									<path
@@ -43,7 +43,7 @@ const Movies = (props: IMovie) => {
 								<svg
 									onDoubleClick={() => handleFavorite(movie.id, movie.favorite)}
 									xmlns="http://www.w3.org/2000/svg"
-									className="h-6 w-6 float-right"
+									className="h-6 w-6 float-right cursor-pointer"
 									fill="none"
 									viewBox="0 0 24 24"
 									stroke="currentColor">
@@ -70,7 +70,7 @@ const Movies = (props: IMovie) => {
 									<svg
 										onDoubleClick={() => handleVote(movie.id, movie.votes)}
 										xmlns="http://www.w3.org/2000/svg"
-										className="h-5 w-5"
+										className="h-5 w-5 cursor-pointer"
 										viewBox="0 0 20 20"
 										fill="blue">
 										<path d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z" />
@@ -84,7 +84,7 @@ const Movies = (props: IMovie) => {
 												handleWatchList(movie.id, movie.watch)
 											}
 											xmlns="http://www.w3.org/2000/svg"
-											className="h-5 w-5"
+											className="h-5 w-5 cursor-pointer"
 											viewBox="0 0 20 20"
 											fill="currentColor">
 											<path
@@ -100,7 +100,7 @@ const Movies = (props: IMovie) => {
 												handleWatchList(movie.id, movie.watch)
 											}
 											xmlns="http://www.w3.org/2000/svg"
-											className="h-6 w-6"
+											className="h-6 w-6 cursor-pointer"
 											fill="none"
 											viewBox="0 0 24 24"
 											stroke="currentColor">
