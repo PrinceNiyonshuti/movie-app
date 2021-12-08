ac8ebb91cc00e5ce1f866feca230882c022bbdde
PrinceNiyonshuti <npprince47@gmail.com>
2021-12-08 08:55:46 +0200

    no changes

--- src/PrivateRoutes.tsx
+++ src/PrivateRoutes.tsx
@@ -1,20 +1,3 @@
-/** @format */
-
-// import { ReactNode, useContext } from "react";
-// import { Route, Link, RouteProps, Navigate } from "react-router-dom";
-// import { AuthContext } from "./context/AuthContext";
-
-// export default function PrivateRoutes({ component: Component, ...rest }) {
-// 	// Context data
-// 	const { currentUser } = useContext(AuthContext);
-// 	return (
-// 		<Route
-// 			{...rest}
-// 			render={(props: JSX.IntrinsicAttributes) => {
-// 				return currentUser ? <Component {...props} /> : <Link to="/" />;
-// 			}}></Route>
-// 	);
-// }
 
 import { FC, useContext } from "react";
 import { Navigate } from "react-router-dom";
